5d29a0544b84f927eba0f74264f850ed7e6b7132
RiardK <142780409+RiardK@users.noreply.github.com>
2023-10-30 00:01:06 +0100

    Add files via upload

---
+++ geometrische_grundlagen/geometrische_grundlagen/Class1.cs
@@ -0,0 +1,62 @@
+﻿class Figur
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public Figur(float x = 100.0f, float y = 100.0f)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public virtual void PrintPosition()
+    {
+        Console.WriteLine($"Position (obere linke Ecke): X={X}, Y={Y}");
+    }
+
+    public virtual void PrintArea()
+    {
+        Console.WriteLine("Fläche: Nicht definiert");
+    }
+}
+
+class Kreis : Figur
+{
+    public float Radius { get; set; }
+
+    public Kreis(float x = 100.0f, float y = 100.0f, float radius = 1.0f)
+        : base(x, y)
+    {
+        Radius = radius;
+    }
+
+    public override void PrintPosition()
+    {
+        Console.WriteLine($"Mittelpunkt: X={X}, Y={Y}");
+    }
+}
+
+class Rechteck : Figur
+{
+    public float Breite { get; set; }
+    public float Höhe { get; set; }
+
+    public Rechteck(float x = 100.0f, float y = 100.0f, float breite = 1.0f, float höhe = 1.0f)
+        : base(x, y)
+    {
+        Breite = breite;
+        Höhe = höhe;
+    }
+
+    public override void PrintPosition()
+    {
+        Console.WriteLine($"Position (obere linke Ecke): X={X}, Y={Y}");
+        Console.WriteLine($"Position (untere rechte Ecke): X={X + Breite}, Y={Y + Höhe}");
+    }
+
+    public override void PrintArea()
+    {
+        Console.WriteLine($"Fläche: {Breite * Höhe}");
+    }
+}
+
---
+++ geometrische_grundlagen/geometrische_grundlagen/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<Figur> figuren = new List<Figur>();
+
+        
+        Kreis kreis = new Kreis(150.0f, 150.0f, 5.0f);
+        Rechteck rechteck = new Rechteck(200.0f, 200.0f, 8.0f, 6.0f);
+
+        figuren.Add(kreis);
+        figuren.Add(rechteck);
+
+        
+        foreach (Figur figur in figuren)
+        {
+            Console.WriteLine($"Figur-Typ: {figur.GetType().Name}");
+            figur.PrintPosition();
+            figur.PrintArea();
+            Console.WriteLine();
+        }
+    }
+}
